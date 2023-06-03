9b208abc903ec5143ca2a53d9b852d62d69b9d62
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-06-03 13:55:45 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { Routes } from 'react-router';
-import '../index.css';
 
 export const App = () => {
   return (
@@ -11,7 +10,6 @@ export const App = () => {
         alignItems: 'center',
         fontSize: 40,
         color: '#010101',
-        fontFamily: 'Coolvetica',
       }}
     >
       React homework template
