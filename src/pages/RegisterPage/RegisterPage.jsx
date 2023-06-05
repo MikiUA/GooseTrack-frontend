c7bd34dec21de25931b32edc79c848b1df0e0017
YanaLiz <lapa.270519@gmail.com>
2023-06-05 13:12:42 +0300

    add

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,2 +1,21 @@
 //RegisterForm
-//AuthNavigate 
+//AuthNavigate
+
+import { Helmet } from 'react-helmet';
+// import { Button } from '@mui/base';
+import RegisterForm from 'components/AuthForms/RegisterForm/RegisterForm';
+// import SectionTitle from 'components/SectionTitle/SectionTitle';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <Helmet>
+        <title>Registration</title>
+       </Helmet>
+      {/* <Button title="Sing Up"></Button> */}
+        <RegisterForm />
+    
+     </div>
+  );
+};
+export default RegisterPage;
