ff09a2953d736aff50988876a33ef17fa6729efa
YanaLiz <lapa.270519@gmail.com>
2023-06-05 14:27:50 +0300

    Видалила svg

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,21 +1,14 @@
 //RegisterForm
 //AuthNavigate
 
-import { Helmet } from 'react-helmet';
-// import { Button } from '@mui/base';
+
 import RegisterForm from 'components/AuthForms/RegisterForm/RegisterForm';
-// import SectionTitle from 'components/SectionTitle/SectionTitle';
+
 
 const RegisterPage = () => {
   return (
-    <div>
-      <Helmet>
-        <title>Registration</title>
-       </Helmet>
-      {/* <Button title="Sing Up"></Button> */}
         <RegisterForm />
-    
-     </div>
+  
   );
 };
 export default RegisterPage;
