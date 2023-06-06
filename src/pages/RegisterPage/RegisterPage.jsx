6f2ea75aa8b1dcf0d4e8b39a8294bbcc74226a04
YanaLiz <lapa.270519@gmail.com>
2023-06-06 23:33:10 +0300

    Зроблена стилізація форми регістрації

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -3,12 +3,24 @@
 
 
 import RegisterForm from 'components/AuthForms/RegisterForm/RegisterForm';
+import { AuthNavigate } from 'components/AuthNavigate';
+import { StyledDiv } from './RegisterPage.styled'; 
+import { StyledImg } from './RegisterPage.styled';
 
 
 const RegisterPage = () => {
   return (
+    <StyledDiv>
+      
         <RegisterForm />
-  
+      <AuthNavigate redirect={'/login'} nameLink="Log In" />
+      
+      <StyledImg
+        src={require('../../images/page/Img/elements.png')}
+        srcset="../../images/page/Img/elements.png, ../../images/page/Img/elements.png 2x"
+        alt="Goose"
+      />
+    </StyledDiv>
   );
 };
 export default RegisterPage;
