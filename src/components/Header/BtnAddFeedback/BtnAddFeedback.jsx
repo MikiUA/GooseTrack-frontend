e70a1b153795f25d874ba535eea079644a3d3ff5
Valentyn Herasymenko <valikgera2002@gmail.com>
2023-06-08 01:42:43 +0300

    Made icon theme depending on themeMode
    Made icon theme depending on themeMode (Потребує подальшого дописання коду для того, хто буде робити зміну темної-світлої теми)

--- src/components/Header/BtnAddFeedback/BtnAddFeedback.jsx
+++ src/components/Header/BtnAddFeedback/BtnAddFeedback.jsx
@@ -1,5 +1,4 @@
 import { StyledButton } from './BtnAddFeedback.styled';
-import { Button } from '@mui/material';
 
 export const BtnAddFeedback = () => {
   return <StyledButton variant="contained">Feedback</StyledButton>;
--- src/components/Header/HeaderPageName/HeaderPageName.jsx
+++ src/components/Header/HeaderPageName/HeaderPageName.jsx
@@ -1,8 +1,12 @@
 import { StyledHeader, StyledIcon } from './HeaderPageName.styled';
 
 export const HeaderPageName = () => {
+  const currentPage = window.location.pathname;
+
   return (
     <div>
+      {currentPage === '/' && <StyledHeader>User Profile</StyledHeader>}
+      {currentPage === '/calendar' && <StyledHeader>Calendar</StyledHeader>}
       <StyledHeader>Hello World</StyledHeader>
       <StyledIcon />
     </div>
