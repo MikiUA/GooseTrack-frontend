f1048605f1b87c34d757ef3f0fec3f7e5cdaec97
Serhii Pushok <sergeypushok1991@gmail.com>
2023-06-07 11:01:22 +0300

    uncomment LoginPage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,26 +1,26 @@
 //LoginForm
 //AuthNanigate
 
-// import LoginForm from '../../components/AuthForms/LoginForm/LoginForm';
-// import { AuthNavigate } from '../../components/AuthNavigate';
-// import { StyledDiv, StyledLoginFormWrapper } from './LoginPage.styled';
-// import { StyledImg } from './LoginPage.styled';
+import LoginForm from '../../components/AuthForms/LoginForm/LoginForm';
+import { AuthNavigate } from '../../components/AuthNavigate';
+import { StyledDiv, StyledLoginFormWrapper } from './LoginPage.styled';
+import { StyledImg } from './LoginPage.styled';
 
-// const LoginPage = () => {
-//   const redirectRegister = '/register';
-//   return (
-//     <StyledDiv>
-//       <StyledLoginFormWrapper>
-//         <LoginForm />
-//         <AuthNavigate redirect={redirectRegister} nameLink="Sign up" />
-//       </StyledLoginFormWrapper>
-//       <StyledImg
-//         src={require('../../images/page/Desktop/gooseinrocket_desk@1x.png')}
-//         srcset="../../images/page/Desktop/gooseinrocket_desk@1x.png, ../../images/page/Desktop/gooseinrocket_desk@2x.png 2x"
-//         alt="Goose"
-//       />
-//     </StyledDiv>
-//   );
-// };
+const LoginPage = () => {
+  const redirectRegister = '/register';
+  return (
+    <StyledDiv>
+      <StyledLoginFormWrapper>
+        <LoginForm />
+        <AuthNavigate redirect={redirectRegister} nameLink="Sign up" />
+      </StyledLoginFormWrapper>
+      <StyledImg
+        src={require('../../images/page/Desktop/gooseinrocket_desk@1x.png')}
+        srcset="../../images/page/Desktop/gooseinrocket_desk@1x.png, ../../images/page/Desktop/gooseinrocket_desk@2x.png 2x"
+        alt="Goose"
+      />
+    </StyledDiv>
+  );
+};
 
-// export default LoginPage;
+export default LoginPage;
