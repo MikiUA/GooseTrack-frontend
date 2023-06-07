a4f94f22234c204d02c5c70e65a265def8c8e1cd
Valentyn Herasymenko <valikgera2002@gmail.com>
2023-06-08 00:06:22 +0300

    made demo version of header
    Made demo version of header, without svg and user`s info (avatar and nickname)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,21 @@
-//BtnAddFeedback
-//ThemeTogger
-//UserInfo
-//AddFeedbackModal
+import { HeaderPageName } from './HeaderPageName/HeaderPageName';
+import { BtnAddFeedback } from './BtnAddFeedback/BtnAddFeedback';
+import { ThemeTogger } from './ThemeTogger/ThemeTogger';
+import { UserInfo } from './UserInfo/UserInfo';
+
+import { StyledBox, StyledWrapper, UserBox } from './Header.styled';
+
+export const Header = () => {
+  return (
+    <StyledBox>
+      <StyledWrapper>
+        <HeaderPageName />
+        <UserBox>
+          <BtnAddFeedback />
+          <ThemeTogger />
+          <UserInfo />
+        </UserBox>
+      </StyledWrapper>
+    </StyledBox>
+  );
+};
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -14,11 +14,11 @@ const LoginPage = () => {
         <LoginForm />
         <AuthNavigate redirect={redirectRegister} nameLink="Sign up" />
       </StyledLoginFormWrapper>
-      <StyledImg
+      {/* <StyledImg
         src={require('../../images/page/Desktop/gooseinrocket_desk@1x.png')}
         srcset="../../images/page/Desktop/gooseinrocket_desk@1x.png, ../../images/page/Desktop/gooseinrocket_desk@2x.png 2x"
         alt="Goose"
-      />
+      /> */}
     </StyledDiv>
   );
 };
