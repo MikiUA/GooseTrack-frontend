c9998e1d6e0d0453d43ecc25b055b1ae1140c2bb
Valentyn Herasymenko <valikgera2002@gmail.com>
2023-06-09 00:13:37 +0300

    Made HeaderPageName for different pages

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -5,6 +5,8 @@ export const StyledBox = styled.div`
     width: 100%;
     max-width: 335px;
     height: 32px;
+    margin-top: 24px;
+    margin-bottom: 64px;
   }
 
   @media screen and (min-width: 768px) {
@@ -17,6 +19,9 @@ export const StyledBox = styled.div`
     width: 100%;
     max-width: 1100px;
     height: 42px;
+
+    margin-top: 40px;
+    margin-bottom: 32px;
   }
 `;
 
--- src/components/Header/HeaderPageName/HeaderPageName.jsx
+++ src/components/Header/HeaderPageName/HeaderPageName.jsx
@@ -1,9 +1,17 @@
 import { StyledHeader, StyledIcon } from './HeaderPageName.styled';
 
 export const HeaderPageName = () => {
+  const pathSegments = window.location.pathname.split('/');
+  const calendarString = pathSegments.find(segment => segment === 'calendar');
+  // console.log(desiredString); // 'calendar'
+
   return (
     <div>
-      <StyledHeader>Hello World</StyledHeader>
+      {calendarString ? (
+        <StyledHeader>Calendar</StyledHeader>
+      ) : (
+        <StyledHeader>User Profile</StyledHeader>
+      )}
       <StyledIcon />
     </div>
   );
