9a0822eadb51f58948c56b76b60ee3344a03b608
Volodymyr-Bednov <vr.bednov@gmail.com>
2023-06-08 18:35:21 +0300

    CalendarPage

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,25 +1,29 @@
 //CalendarToolbar
 //ChoosedDay
 //ChoosedMonth
-import { useParams } from "react-router"
+import ChoosedMonth from 'components/ChoosedMonth/ChoosedMonth';
+import { useParams } from 'react-router';
 
 const CalendarPage = ({ variant }) => {
-    const { currentDate } = useParams();
-    console.log(currentDate);
-    return <>
-        <h1>CalendarPage</h1>
-        {/* CalendarToolBar (currentDate)*/}
-        {variant === 'month' ?
-            <>
-                <h2>month</h2>
-                {/* ChoosedMonth (currentDate)*/}
-            </>
-            : <>
-                <h2>day</h2>
-                {/* ChoosedDay */}
-            </>
-        }
+  const { currentDate } = useParams();
+  console.log(currentDate);
+  return (
+    <>
+      <h1>CalendarPage</h1>
+      {/* CalendarToolBar (currentDate)*/}
+      {variant === 'month' ? (
+        <>
+          <h2>month</h2>
+          <ChoosedMonth></ChoosedMonth>
+        </>
+      ) : (
+        <>
+          <h2>day</h2>
+          {/* ChoosedDay */}
+        </>
+      )}
     </>
-}
+  );
+};
 
-export default CalendarPage
+export default CalendarPage;
