df20c7791422acd124c5f94fb9648ef728af2019
MikiUA <106928450+MikiUA@users.noreply.github.com>
2023-06-08 16:26:18 +0300

    mui buttons now don't change color

--- src/components/MUIThemeProvider/ThemeSwitcher.jsx
+++ src/components/MUIThemeProvider/ThemeSwitcher.jsx
@@ -1,14 +1,27 @@
 import { useTheme } from "@mui/material/styles";
 import { useToggleDarkMode } from "./MUIThemeProvider";
-import { StyledToggleThemeBox } from "./TestingPage.styled";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
 export default function ThemeSwitcher() {
     const theme = useTheme();
     const toggleDarkMode = useToggleDarkMode();
     return (
-        <StyledToggleThemeBox>
-            {theme.palette.mode} mode <Button onClick={toggleDarkMode} variant="outlined"> switch </Button>
-        </StyledToggleThemeBox>
+        <Box
+            sx={{
+                display: 'block',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                bgcolor: 'background.default',
+                color: 'text.primary',
+                borderRadius: 1,
+                p: 3,
+                border: '1px solid',
+                borderColor: 'palette.primary'
+            }}>
+            {theme.palette.mode} mode
+            <Button onClick={toggleDarkMode} variant="contained"> switch </Button>
+        </Box>
     )
 }
