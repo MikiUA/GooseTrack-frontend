5dc4230e6d13f06b6312fb7b7aa31dbe41bfa38d
Vitalij1989 <108751440+Vitalij1989@users.noreply.github.com>
2023-06-10 00:02:50 +0300

    Сітебар
    Привіт
    Переробів лінки, поправив стилі,
    маю проюлеми з свг, не можу коректно них вставити. Завтра попробую добити

---
+++ src/components/SideBar/UserNav/UserNav.jsx
@@ -0,0 +1,19 @@
+// import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+// import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+// import { UserNavItem, UserNavTitle, UserNavWrap } from './UserNav.styled';
+
+// export const UserNav = () => {
+//   return (
+//     <UserNavWrap>
+//       <UserNavTitle>User Panel </UserNavTitle>
+//       <UserNavItem to="/account">
+//         <PersonAddAltIcon style={{ marginRight: 8, width: 20, height: 20 }} />{' '}
+//         My account
+//       </UserNavItem>
+//       <UserNavItem to="/calendar">
+//         <CalendarMonthIcon style={{ marginRight: 8, width: 20, height: 20 }} />{' '}
+//         Calendar
+//       </UserNavItem>
+//     </UserNavWrap>
+//   );
+// };
