65dba8a7c71cf2a0d183c54a202921a0fdaacecf
MikiUA <106928450+MikiUA@users.noreply.github.com>
2023-06-10 22:31:41 +0300

    simplified calendarroute

--- src/components/Router/CalendarRoute.jsx
+++ src/components/Router/CalendarRoute.jsx
@@ -7,34 +7,29 @@ const CalendarPage = lazy(() =>
 
 function isValidDate(inputDate) {
   const inputFormat = /^\d{4}-\d{2}-\d{2}$/; // Regular expression for 'YYYY-MM-DD' format
-  return (inputFormat.test(inputDate));
-}
-
-function validateDate(inputDate) {
-  const inputFormat = /^\d{4}-\d{2}-\d{2}$/; // Regular expression for 'YYYY-MM-DD' format
-  const currentDate = new Date().toISOString().slice(0, 10); // Get current date in 'YYYY-MM-DD' format
   if (inputFormat.test(inputDate)) {
     const parsedDate = new Date(inputDate);
     if (!isNaN(parsedDate)) {
-      return inputDate; // Input date is valid, so return it
+      return true; // Input date is valid
     }
   }
 
-  return currentDate; // Input date is not valid, so return the current date
+  return false; // Input date is not valid
 }
 
 export const CalendarRoute = () => {
   const { currentDate } = useParams();
   const path = useLocation();
+
   const navigateTo = useMemo(() => {
     if (isValidDate(currentDate)) return null;
-    const validDate = validateDate(currentDate);
-    if (path.pathname.includes('/day')) return `/calendar/day/${validateDate(validDate)}`
-    return `/calendar/month/${validDate}`
+    const currentDate = new Date().toISOString().slice(0, 10); // Get current date in 'YYYY-MM-DD' format
+
+    if (path.pathname.includes('/day')) return `/calendar/day/${currentDate}`
+    return `/calendar/month/${currentDate}`
   }, [currentDate, path]);
-  // return <CalendarPage />
+
   return navigateTo
     ? <Navigate to={navigateTo} replace />
     : <CalendarPage variant={path.pathname.includes('/day') ? 'day' : 'month'} />
-
 };
