1ab6bca93e278d8b5a25a1e693cea13112770801
MikiUA <106928450+MikiUA@users.noreply.github.com>
2023-06-12 07:46:14 +0000

    Deploying to gh-pages from @ MikiUA/GooseTrack-frontend@2c397fe480889d840e00ea277c53d20cbbc1b05a 🚀

---
+++ static/js/853.a12f7847.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoose_track_frontend=self.webpackChunkgoose_track_frontend||[]).push([[853],{3853:function(t,n,r){r.d(n,{_fJ:function(){return o}});var e=r(9983);function o(t){return(0,e.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{d:"M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"}},{tag:"polyline",attr:{points:"10 17 15 12 10 7"}},{tag:"line",attr:{x1:"15",y1:"12",x2:"3",y2:"12"}}]})(t)}}}]);
+//# sourceMappingURL=853.a12f7847.chunk.js.map
