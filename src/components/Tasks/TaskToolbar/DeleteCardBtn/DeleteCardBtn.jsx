eacce26e3c9e190e6d3a258775e0e554b2a28469
Vladyslav Sanhinov <108656455+Skynet4241@users.noreply.github.com>
2023-06-13 12:15:36 +0300

    fix dlete func

--- src/components/Tasks/TaskToolbar/DeleteCardBtn/DeleteCardBtn.jsx
+++ src/components/Tasks/TaskToolbar/DeleteCardBtn/DeleteCardBtn.jsx
@@ -1,37 +1,38 @@
-import { useState } from "react";
-import Modal from "components/Modal/Modal";
-import DeleteModalCard from "./DeleteModalCard/DeleteModalCard";
-import { useDeleteTaskByIdMutation } from "API/taskUtils";
-import { Button, IconStyled } from "./DeleteCardBtn.styled";
-
+import { useState } from 'react';
+import Modal from 'components/Modal/Modal';
+import DeleteModalCard from './DeleteModalCard/DeleteModalCard';
+import { useDeleteTaskByIdMutation } from 'API/taskUtils';
+import { Button, IconStyled } from './DeleteCardBtn.styled';
 
 const DeleteCardBtn = ({ id }) => {
-    const [isModalOpen, setIsOpenModal] = useState(false);
-    const handleToggle = () => setIsOpenModal(pS => !pS);
+  const [isModalOpen, setIsOpenModal] = useState(false);
+  const handleToggle = () => setIsOpenModal(pS => !pS);
 
-    const [deleteTask] = useDeleteTaskByIdMutation();
+  const [deleteTask] = useDeleteTaskByIdMutation();
 
-    const handleDelete = async ({ task_id }) => {
-        try {
-            console.log(task_id)
-            const res = await deleteTask( task_id )
-            console.log(res)
-
-        } catch (error) {
-            console.error(error);
-        }
+  const handleDelete = async id => {
+    try {
+      const res = await deleteTask(id);
+      console.log(res);
+    } catch (error) {
+      console.error(error);
     }
+  };
 
-    return (
-        <li>
-            <Button onClick={handleToggle}>
-                <IconStyled name="icon-trash-04" width="16" height="16" />
-            </Button>
-            <Modal onClose={handleToggle} isOpen={isModalOpen}>
-                <DeleteModalCard onClose={handleToggle} handleDelete={() => handleDelete(id)} id={id}/>
-            </Modal>
-        </li>
-    )
-}
+  return (
+    <li>
+      <Button onClick={handleToggle}>
+        <IconStyled name="icon-trash-04" width="16" height="16" />
+      </Button>
+      <Modal onClose={handleToggle} isOpen={isModalOpen}>
+        <DeleteModalCard
+          onClose={handleToggle}
+          handleDelete={() => handleDelete(id)}
+          id={id}
+        />
+      </Modal>
+    </li>
+  );
+};
 
 export default DeleteCardBtn;
--- src/components/Tasks/TaskToolbar/MoveCardBtn/MoveCardBtn.jsx
+++ src/components/Tasks/TaskToolbar/MoveCardBtn/MoveCardBtn.jsx
@@ -1,14 +1,19 @@
-import { Button, IconStyled } from "./MoveCardBtn.styled";
+import { useUpdateTaskByIdMutation } from 'API/taskUtils';
+import { Button, IconStyled } from './MoveCardBtn.styled';
 
+const MoveCardBtn = ({ id }) => {
+  const [updateTaskById] = useUpdateTaskByIdMutation();
+  return (
+    <li>
+      <Button>
+        <IconStyled
+          name="icon-arrow-circle-broken-right"
+          width="16"
+          height="16"
+        />
+      </Button>
+    </li>
+  );
+};
 
-const MoveCardBtn = () => {
-    return (
-        <li>
-            <Button>
-                <IconStyled name="icon-arrow-circle-broken-right" width="16" height="16"/>
-            </Button>
-        </li>
-    )
-}
-
-export default MoveCardBtn
+export default MoveCardBtn;
