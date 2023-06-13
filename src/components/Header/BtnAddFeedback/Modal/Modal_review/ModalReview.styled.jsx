5d088ee36689fb254d471364f14b357edd06bbe7
Valentyn Herasymenko <valikgera2002@gmail.com>
2023-06-13 16:53:23 +0300

    Made feedbackmodal styles less 375

--- src/components/Header/BtnAddFeedback/Modal/Modal_review/ModalReview.styled.jsx
+++ src/components/Header/BtnAddFeedback/Modal/Modal_review/ModalReview.styled.jsx
@@ -2,29 +2,28 @@ import { Box, TextField, styled } from '@mui/material';
 
 export const StyledTextField = styled(TextField)`
   && .MuiInputBase-root {
-    @media screen and (min-width: 375px) {
-      width: 295px;
-      height: 111px;
-      background: #f7f7f7;
-      border-radius: 8px;
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 600;
-      font-size: 14px;
-      line-height: 18px;
+    width: 395px;
+    height: 127px;
+    background: #f7f7f7;
+    border-radius: 8px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 18px;
+
+    @media screen and (max-width: 375px) {
+      width: 300px;
     }
 
-    @media screen and (min-width: 768px) {
-      width: 404px;
-      height: 127px;
+    @media screen and (min-width: 375px) and (max-width: 465px) {
+      width: 320px;
     }
   }
 `;
 
 export const StyledReviewBox = styled(Box)`
-  @media screen and (min-width: 375px) {
-    margin-bottom: 14px;
-  }
+  margin-bottom: 14px;
 
   @media screen and (min-width: 768px) {
     margin-bottom: 18px;
